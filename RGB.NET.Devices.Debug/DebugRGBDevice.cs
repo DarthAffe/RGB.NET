e78525118f39c0ba40241a7711e2a18dfee22eae
Darth Affe <darthaffe@wyrez.org>
2018-01-17 22:23:20 +0100

    Added debug-device-provider

---
+++ RGB.NET.Devices.Debug/DebugRGBDevice.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using RGB.NET.Core;
+using RGB.NET.Core.Layout;
+
+namespace RGB.NET.Devices.Debug
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Represents a debug device.
+    /// </summary>
+    public class DebugRGBDevice : AbstractRGBDevice<DebugRGBDeviceInfo>
+    {
+        #region Properties & Fields
+
+        /// <inheritdoc />
+        public override DebugRGBDeviceInfo DeviceInfo { get; }
+
+        private Func<Dictionary<LedId, Color>> _syncBackFunc;
+        private Action<IEnumerable<Led>> _updateLedsAction;
+
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Internal constructor of <see cref="DebugRGBDeviceInfo"/>.
+        /// </summary>
+        internal DebugRGBDevice(string layoutPath, Func<Dictionary<LedId, Color>> syncBackFunc = null, Action<IEnumerable<Led>> updateLedsAction = null)
+        {
+            this._syncBackFunc = syncBackFunc;
+            this._updateLedsAction = updateLedsAction;
+
+            DeviceLayout layout = DeviceLayout.Load(layoutPath);
+            DeviceInfo = new DebugRGBDeviceInfo(layout.Type, layout.Vendor, layout.Model, layout.Lighting, syncBackFunc != null);
+        }
+
+        #endregion
+
+        #region Methods
+
+        internal void Initialize(string layoutPath, string imageLayout) => ApplyLayoutFromFile(layoutPath, imageLayout, true);
+
+        /// <inheritdoc />
+        public override void SyncBack()
+        {
+            try
+            {
+                Dictionary<LedId, Color> syncBackValues = _syncBackFunc?.Invoke();
+                if (syncBackValues == null) return;
+
+                foreach (KeyValuePair<LedId, Color> value in syncBackValues)
+                {
+                    Led led = ((IRGBDevice)this)[value.Key];
+                    if (led != null)
+                        led.Color = value.Value;
+                }
+            }
+            catch {/* ics that's not my fault ... */}
+        }
+
+        /// <inheritdoc />
+        protected override void UpdateLeds(IEnumerable<Led> ledsToUpdate) => _updateLedsAction?.Invoke(ledsToUpdate);
+
+        #endregion
+    }
+}
---
+++ RGB.NET.Devices.Debug/DebugRGBDeviceInfo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using RGB.NET.Core;
+
+namespace RGB.NET.Devices.Debug
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Represents device information for a <see cref="DebugRGBDevice"/> />.
+    /// </summary>
+    public class DebugRGBDeviceInfo : IRGBDeviceInfo
+    {
+        #region Properties & Fields
+
+        /// <inheritdoc />
+        public RGBDeviceType DeviceType { get; }
+
+        /// <inheritdoc />
+        public string Manufacturer { get; }
+
+        /// <inheritdoc />
+        public string Model { get; }
+
+        /// <inheritdoc />
+        public RGBDeviceLighting Lighting { get; }
+
+        /// <inheritdoc />
+        public bool SupportsSyncBack { get; }
+
+        /// <inheritdoc />
+        public Uri Image { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Internal constructor of <see cref="DebugRGBDeviceInfo"/>.
+        /// </summary>
+        /// <param name="deviceType">The <see cref="RGBDeviceType"/> of the device.</param>
+        /// <param name="manufacturer">The manufacturer of the device.</param>
+        /// <param name="model">The model of the device.</param>
+        /// <param name="lighting">The <see cref="RGBDeviceLighting"/> of the device.</param>
+        /// <param name="supportsSyncBack">True if the device supports syncback; false if not.</param>
+        internal DebugRGBDeviceInfo(RGBDeviceType deviceType, string manufacturer, string model, RGBDeviceLighting lighting, bool supportsSyncBack)
+        {
+            this.DeviceType = deviceType;
+            this.Manufacturer = manufacturer;
+            this.Model = model;
+            this.Lighting = lighting;
+            this.SupportsSyncBack = supportsSyncBack;
+        }
+
+        #endregion
+    }
+}
