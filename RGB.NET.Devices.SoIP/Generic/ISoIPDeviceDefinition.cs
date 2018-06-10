4b2aba844732a549a5b4f6903c30923f4cafd91c
Darth Affe <darthaffe@wyrez.org>
2018-06-10 22:01:03 +0200

    Added SoIP device-provider (WIP)

---
+++ RGB.NET.Devices.SoIP/Generic/ISoIPDeviceDefinition.cs
@@ -0,0 +1,8 @@
+﻿namespace RGB.NET.Devices.SoIP.Generic
+{
+    /// <summary>
+    /// Marker interface for SoIP device definitions.
+    /// </summary>
+    public interface ISoIPDeviceDefinition
+    { }
+}
---
+++ RGB.NET.Devices.SoIP/Generic/ISoIPRGBDevice.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using RGB.NET.Core;
+
+namespace RGB.NET.Devices.SoIP.Generic
+{
+    // ReSharper disable once InconsistentNaming
+    internal interface ISoIPRGBDevice : IRGBDevice, IDisposable
+    {
+        void Initialize(IDeviceUpdateTrigger updateTrigger);
+    }
+}
