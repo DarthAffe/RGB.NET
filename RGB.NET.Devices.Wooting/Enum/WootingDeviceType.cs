1bdbc7b021181c495603d6ef99f57543207db1a0
SpoinkyNL <spoinky.nl@gmail.com>
2020-01-28 19:43:15 +0100

    Updated for official v1.1.0 Wooting SDK

---
+++ RGB.NET.Devices.Wooting/Enum/WootingDeviceType.cs
@@ -0,0 +1,11 @@
+﻿namespace RGB.NET.Devices.Wooting.Enum
+{
+    public enum WootingDeviceType
+    {
+        /// 10 Keyless Keyboard. E.g. Wooting One
+        KeyboardTKL = 1,
+
+        /// Full Size keyboard. E.g. Wooting Two
+        Keyboard = 2
+    }
+}
