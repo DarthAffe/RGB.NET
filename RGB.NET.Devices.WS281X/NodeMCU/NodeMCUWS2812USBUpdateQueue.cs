7c6db8738c58c11777cc4fe105f83c6a74d04db2
Darth Affe <darthaffe@wyrez.org>
2020-07-12 16:06:54 +0200

    Added device-provider for NodeMCU based WS281X-leds

---
+++ RGB.NET.Devices.WS281X/NodeMCU/NodeMCUWS2812USBUpdateQueue.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using RGB.NET.Core;
+
+namespace RGB.NET.Devices.WS281X.NodeMCU
+{
+    // ReSharper disable once InconsistentNaming
+    /// <inheritdoc />
+    /// <summary>
+    /// Represents the update-queue performing updates for NodeMCU WS2812 devices.
+    /// </summary>
+    public class NodeMCUWS2812USBUpdateQueue : UpdateQueue
+    {
+        #region Constants
+
+        private static readonly byte UPDATE_COMMAND = 0x02;
+
+        #endregion
+
+        #region Properties & Fields
+
+        private readonly string _hostname;
+
+        private readonly UdpClient _udpClient;
+        private readonly Dictionary<int, byte[]> _dataBuffer = new Dictionary<int, byte[]>();
+        private readonly Dictionary<int, byte> _sequenceNumbers = new Dictionary<int, byte>();
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NodeMCUWS2812USBUpdateQueue"/> class.
+        /// </summary>
+        /// <param name="updateTrigger">The update trigger used by this queue.</param>
+        /// <param name="hostname">The hostname to connect to.</param>
+        /// <param name="port">The port used by the web-connection.</param>
+        public NodeMCUWS2812USBUpdateQueue(IDeviceUpdateTrigger updateTrigger, string hostname, int port)
+            : base(updateTrigger)
+        {
+            this._hostname = hostname;
+
+            _udpClient = new UdpClient(_hostname, port);
+            _udpClient.Connect(_hostname, port);
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc />
+        protected override void Update(Dictionary<object, Color> dataSet)
+        {
+            foreach (IGrouping<int, ((int channel, int key), Color Value)> channelData in dataSet.Select(x => (((int channel, int key))x.Key, x.Value))
+                                                                                                 .GroupBy(x => x.Item1.channel))
+            {
+                int channel = channelData.Key;
+                byte[] buffer = _dataBuffer[channel];
+
+                buffer[0] = GetSequenceNumber(channel);
+                buffer[1] = (byte)((channel << 4) | UPDATE_COMMAND);
+                int i = 2;
+                foreach ((byte _, byte r, byte g, byte b) in channelData.OrderBy(x => x.Item1.key)
+                                                                        .Select(x => x.Value.GetRGBBytes()))
+                {
+                    buffer[i++] = r;
+                    buffer[i++] = g;
+                    buffer[i++] = b;
+                }
+
+                Send(buffer);
+            }
+        }
+
+        internal IEnumerable<(int channel, int ledCount)> GetChannels()
+        {
+            WebClient webClient = new WebClient();
+            webClient.DownloadString($"http://{_hostname}/reset");
+
+            int channelCount = int.Parse(webClient.DownloadString($"http://{_hostname}/channels"));
+            for (int channel = 1; channel <= channelCount; channel++)
+            {
+                int ledCount = int.Parse(webClient.DownloadString($"http://{_hostname}/channel/{channel}"));
+                if (ledCount > 0)
+                {
+                    _dataBuffer[channel] = new byte[(ledCount * 3) + 2];
+                    _sequenceNumbers[channel] = 0;
+                    yield return (channel, ledCount);
+                }
+            }
+        }
+
+        private void Send(byte[] data) => _udpClient.Send(data, data.Length);
+
+        private byte GetSequenceNumber(int channel)
+        {
+            byte sequenceNumber = (byte)((_sequenceNumbers[channel] + 1) % byte.MaxValue);
+            _sequenceNumbers[channel] = sequenceNumber;
+            return sequenceNumber;
+        }
+
+        #endregion
+    }
+}
---
+++ RGB.NET.Devices.WS281X/NodeMCU/NodeMCUWS281XDeviceDefinition.cs
@@ -0,0 +1,71 @@
+﻿// ReSharper disable MemberCanBePrivate.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable AutoPropertyCanBeMadeGetOnly.Global
+
+using System.Collections.Generic;
+using RGB.NET.Core;
+
+namespace RGB.NET.Devices.WS281X.NodeMCU
+{
+    // ReSharper disable once InconsistentNaming
+    /// <inheritdoc />
+    /// <summary>
+    /// Represents a definition of an NodeMCU WS2812 devices.
+    /// </summary>
+    public class NodeMCUWS281XDeviceDefinition : IWS281XDeviceDefinition
+    {
+        #region Properties & Fields
+
+        /// <summary>
+        /// Gets the hostname to connect to.
+        /// </summary>
+        public string Hostname { get; }
+
+        /// <summary>
+        /// Gets the port of the UDP connection.
+        /// </summary>
+        public int Port { get; }
+
+        /// <summary>
+        /// Gets or sets the name used by this device.
+        /// This allows to use {0} as a placeholder for a incrementing number if multiple devices are created.
+        /// </summary>
+        public string Name { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NodeMCUWS281XDeviceDefinition"/> class.
+        /// </summary>
+        /// <param name="hostname">The hostname to connect to.</param>
+        /// <param name="port">The port of the UDP connection.</param>
+        public NodeMCUWS281XDeviceDefinition(string hostname, int port = 1872)
+        {
+            this.Hostname = hostname;
+            this.Port = port;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc />
+        public IEnumerable<IRGBDevice> CreateDevices(IDeviceUpdateTrigger updateTrigger)
+        {
+            NodeMCUWS2812USBUpdateQueue queue = new NodeMCUWS2812USBUpdateQueue(updateTrigger, Hostname, Port);
+            IEnumerable<(int channel, int ledCount)> channels = queue.GetChannels();
+            int counter = 0;
+            foreach ((int channel, int ledCount) in channels)
+            {
+                string name = string.Format(Name ?? $"NodeMCU WS2812 WIFI ({Hostname}) [{{0}}]", ++counter);
+                NodeMCUWS2812USBDevice device = new NodeMCUWS2812USBDevice(new NodeMCUWS2812USBDeviceInfo(name), queue, channel);
+                device.Initialize(ledCount);
+                yield return device;
+            }
+        }
+
+        #endregion
+    }
+}
