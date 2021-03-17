f03ae301aa5cab14d48bc9da29a1490b8b0304fc
Robert <mail@rbeekman.nl>
2021-03-17 22:27:42 +0100

    Razer - Added endpoint types to device info
    Razer - Only add the first device of each endpoint type
    Razer - Fixed mouse effect ID

--- RGB.NET.Devices.Razer/Generic/RazerRGBDeviceInfo.cs
+++ RGB.NET.Devices.Razer/Generic/RazerRGBDeviceInfo.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using RGB.NET.Core;
+﻿using RGB.NET.Core;
 
 namespace RGB.NET.Devices.Razer
 {
@@ -26,6 +25,11 @@ namespace RGB.NET.Devices.Razer
         /// <inheritdoc />
         public object? LayoutMetadata { get; set; }
 
+        /// <summary>
+        /// Gets the Razer SDK endpoint type the <see cref="IRGBDevice"/> is addressed through.
+        /// </summary>
+        public RazerEndpointType EndpointType { get; }
+
         #endregion
 
         #region Constructors
@@ -34,10 +38,12 @@ namespace RGB.NET.Devices.Razer
         /// Internal constructor of managed <see cref="RazerRGBDeviceInfo"/>.
         /// </summary>
         /// <param name="deviceType">The type of the <see cref="IRGBDevice"/>.</param>
+        /// <param name="endpointType">The Razer SDK endpoint type the <see cref="IRGBDevice"/> is addressed through.</param>
         /// <param name="model">The model of the <see cref="IRGBDevice"/>.</param>
-        internal RazerRGBDeviceInfo(RGBDeviceType deviceType, string model)
+        internal RazerRGBDeviceInfo(RGBDeviceType deviceType, RazerEndpointType endpointType, string model)
         {
             this.DeviceType = deviceType;
+            this.EndpointType = endpointType;
             this.Model = model;
 
             DeviceName = $"{Manufacturer} {Model}";
--- RGB.NET.Devices.Razer/Native/_Defines.cs
+++ RGB.NET.Devices.Razer/Native/_Defines.cs
@@ -7,7 +7,7 @@
         internal const int CHROMALINK_EFFECT_ID = 1;
         internal const int KEYBOARD_EFFECT_ID = 9;
         internal const int MOUSEPAD_EFFECT_ID = 6;
-        internal const int MOUSE_EFFECT_ID = 3;
+        internal const int MOUSE_EFFECT_ID = 8;
         internal const int KEYPAD_EFFECT_ID = 2;
 
         internal const int KEYBOARD_MAX_ROW = 8;
@@ -16,7 +16,7 @@
 
         internal const int MOUSE_MAX_ROW = 9;
         internal const int MOUSE_MAX_COLUMN = 7;
-        internal const int MOUSE_MAX_LEDS = KEYBOARD_MAX_ROW * KEYBOARD_MAX_COLUMN;
+        internal const int MOUSE_MAX_LEDS = MOUSE_MAX_ROW * MOUSE_MAX_COLUMN;
 
         internal const int HEADSET_MAX_LEDS = 5;
 
