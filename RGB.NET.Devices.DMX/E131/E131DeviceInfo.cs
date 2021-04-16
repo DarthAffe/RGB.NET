4ce2562374df042f18ca73626161955fe3a3698c
Robert <mail@rbeekman.nl>
2021-04-16 17:37:11 +0200

    DMX - Fix default random CID creation

--- RGB.NET.Devices.DMX/E131/E131DeviceInfo.cs
+++ RGB.NET.Devices.DMX/E131/E131DeviceInfo.cs
@@ -69,10 +69,10 @@ namespace RGB.NET.Devices.DMX.E131
             this.Universe = deviceDefinition.Universe;
 
             byte[]? cid = deviceDefinition.CID;
-            if ((CID == null) || (CID.Length != CID_LENGTH))
+            if ((cid == null) || (cid.Length != CID_LENGTH))
             {
-                CID = new byte[CID_LENGTH];
-                new Random().NextBytes(CID);
+                cid = new byte[CID_LENGTH];
+                new Random().NextBytes(cid);
             }
 
             CID = cid!;
