5d3071235b51ed70fb96aaf5fc7195fac443b7e4
Darth Affe <darthaffe@wyrez.org>
2021-04-24 20:41:55 +0200

    Added PicoPi device provider

---
+++ RGB.NET.Devices.PicoPi/PicoPi/LedMappings.cs
@@ -0,0 +1,23 @@
+﻿using RGB.NET.Core;
+
+namespace RGB.NET.Devices.PicoPi
+{
+    public static class LedMappings
+    {
+        #region Properties & Fields
+
+        public static LedMapping<int> StripeMapping = new();
+
+        #endregion
+
+        #region Constructors
+
+        static LedMappings()
+        {
+            for (int i = 0; i < 255; i++)
+                StripeMapping.Add(LedId.LedStripe1 + i, i);
+        }
+
+        #endregion
+    }
+}
