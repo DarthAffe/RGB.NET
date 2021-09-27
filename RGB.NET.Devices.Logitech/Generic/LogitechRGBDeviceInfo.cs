0edf2fc7d6c62f31343a2f7eff07b7af58d61c2b
Robert <mail@rbeekman.nl>
2021-09-27 21:44:19 +0200

    Swap the positions and make zoneOffset optional

--- RGB.NET.Devices.Logitech/Generic/LogitechRGBDeviceInfo.cs
+++ RGB.NET.Devices.Logitech/Generic/LogitechRGBDeviceInfo.cs
@@ -31,14 +31,14 @@ namespace RGB.NET.Devices.Logitech
         public LogitechDeviceCaps DeviceCaps { get; }
 
         /// <summary>
-        /// Gets the zone at which LEDs start being mapped
-        /// </summary>
-        public int ZoneOffset { get; }
-
-        /// <summary>
         /// Gets the amount of zones the <see cref="LogitechRGBDevice{TDeviceInfo}"/> is able to control (0 for single-color and per-key devices)
         /// </summary>
         public int Zones { get; }
+
+        /// <summary>
+        /// Gets the zone at which LEDs start being mapped
+        /// </summary>
+        public int ZoneOffset { get; }
 
         #endregion
 
@@ -50,15 +50,15 @@ namespace RGB.NET.Devices.Logitech
         /// <param name="deviceType">The type of the <see cref="IRGBDevice"/>.</param>
         /// <param name="model">The represented device model.</param>
         /// <param name="deviceCaps">The lighting-capabilities of the device.</param>
-        /// <param name="zoneOffset">The zone at which to start mapping LEDs.</param>
         /// <param name="zones">The amount of zones the device is able to control.</param>
-        internal LogitechRGBDeviceInfo(RGBDeviceType deviceType, string model, LogitechDeviceCaps deviceCaps, int zoneOffset, int zones)
+        /// <param name="zoneOffset">The zone at which to start mapping LEDs.</param>
+        internal LogitechRGBDeviceInfo(RGBDeviceType deviceType, string model, LogitechDeviceCaps deviceCaps, int zones, int zoneOffset = 0)
         {
             this.DeviceType = deviceType;
             this.Model = model;
             this.DeviceCaps = deviceCaps;
-            this.ZoneOffset = zoneOffset;
             this.Zones = zones;
+            this.ZoneOffset = zoneOffset;
 
             DeviceName = DeviceHelper.CreateDeviceName(Manufacturer, Model);
         }
