293343a7ed9ef99ccbc912b6cc99d155456021b1
Darth Affe <darthaffe@wyrez.org>
2021-10-07 23:14:21 +0200

    Added a workaround for a SDK-issue with keypad-5

--- RGB.NET.Devices.SteelSeries/API/SteelSeriesSDK.cs
+++ RGB.NET.Devices.SteelSeries/API/SteelSeriesSDK.cs
@@ -44,7 +44,9 @@ namespace RGB.NET.Devices.SteelSeries.API
 (add-event-zone-use-with-specifier ""{EVENT_NAME}"" ""all"" ""rgb-12-zone"")
 (add-event-zone-use-with-specifier ""{EVENT_NAME}"" ""all"" ""rgb-17-zone"")
 (add-event-zone-use-with-specifier ""{EVENT_NAME}"" ""all"" ""rgb-24-zone"")
-(add-event-zone-use-with-specifier ""{EVENT_NAME}"" ""all"" ""rgb-103-zone"")";
+(add-event-zone-use-with-specifier ""{EVENT_NAME}"" ""all"" ""rgb-103-zone"")
+
+(add-custom-zone '(""num-5"" 93))"; //HACK DarthAffe 07.10.2021: Custom zone to workaround a SDK-issue (https://github.com/SteelSeries/gamesense-sdk/issues/85)
 
         private const string CORE_PROPS_WINDOWS = "%PROGRAMDATA%/SteelSeries/SteelSeries Engine 3/coreProps.json";
         private const string CORE_PROPS_OSX = "/Library/Application Support/SteelSeries Engine 3/coreProps.json";
