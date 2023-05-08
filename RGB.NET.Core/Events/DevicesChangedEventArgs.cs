7e72d3199beefca076fd12bbba2802b4ba2940e3
Darth Affe <darthaffe@wyrez.org>
2023-05-08 21:28:55 +0200

    Added DevicesChanged-event to device providers

---
+++ RGB.NET.Core/Events/DevicesChangedEventArgs.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RGB.NET.Core;
+
+public sealed class DevicesChangedEventArgs : EventArgs
+{
+    #region Properties & Fields
+
+    public IList<IRGBDevice> Added { get; }
+    public IList<IRGBDevice> Removed { get; }
+
+    #endregion
+
+    #region Constructors
+
+    private DevicesChangedEventArgs(IList<IRGBDevice> added, IList<IRGBDevice> removed)
+    {
+        this.Added = added;
+        this.Removed = removed;
+    }
+
+    #endregion
+
+    #region Methods
+
+    public static DevicesChangedEventArgs CreateDevicesAddedArgs(params IRGBDevice[] addedDevices) => CreateDevicesAddedArgs((IEnumerable<IRGBDevice>)addedDevices);
+    public static DevicesChangedEventArgs CreateDevicesAddedArgs(IEnumerable<IRGBDevice> addedDevices) => new(new List<IRGBDevice>(addedDevices), new List<IRGBDevice>());
+
+    public static DevicesChangedEventArgs CreateDevicesRemovedArgs(params IRGBDevice[] removedDevices) => CreateDevicesRemovedArgs((IEnumerable<IRGBDevice>)removedDevices);
+    public static DevicesChangedEventArgs CreateDevicesRemovedArgs(IEnumerable<IRGBDevice> removedDevices) => new(new List<IRGBDevice>(), new List<IRGBDevice>(removedDevices));
+
+    #endregion
+}
