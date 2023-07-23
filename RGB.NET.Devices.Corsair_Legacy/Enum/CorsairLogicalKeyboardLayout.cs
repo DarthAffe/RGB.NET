6f91af903974ac745915fd41b9d45301c9e64a0b
Darth Affe <darthaffe@wyrez.org>
2023-07-23 19:49:30 +0200

    Readded support for older CUE-SDKs

---
+++ RGB.NET.Devices.Corsair_Legacy/Enum/CorsairLogicalKeyboardLayout.cs
@@ -0,0 +1,30 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable UnusedMember.Global
+#pragma warning disable 1591
+
+namespace RGB.NET.Devices.CorsairLegacy;
+
+/// <summary>
+/// Contains a list of available logical layouts for corsair keyboards.
+/// </summary>
+public enum CorsairLogicalKeyboardLayout
+{
+    US_Int = 1,
+    NA = 2,
+    EU = 3,
+    UK = 4,
+    BE = 5,
+    BR = 6,
+    CH = 7,
+    CN = 8,
+    DE = 9,
+    ES = 10,
+    FR = 11,
+    IT = 12,
+    ND = 13,
+    RU = 14,
+    JP = 15,
+    KR = 16,
+    TW = 17,
+    MEX = 18
+};
---
+++ RGB.NET.Devices.Corsair_Legacy/Mousepad/CorsairMousepadRGBDeviceInfo.cs
@@ -0,0 +1,25 @@
+﻿using RGB.NET.Core;
+using RGB.NET.Devices.CorsairLegacy.Native;
+
+namespace RGB.NET.Devices.CorsairLegacy;
+
+/// <inheritdoc />
+/// <summary>
+/// Represents a generic information for a <see cref="T:RGB.NET.Devices.CorsairLegacy.CorsairMousepadRGBDevice" />.
+/// </summary>
+public class CorsairMousepadRGBDeviceInfo : CorsairRGBDeviceInfo
+{
+    #region Constructors
+
+    /// <inheritdoc />
+    /// <summary>
+    /// Internal constructor of managed <see cref="T:RGB.NET.Devices.CorsairLegacy.CorsairMousepadRGBDeviceInfo" />.
+    /// </summary>
+    /// <param name="deviceIndex">The index if the <see cref="T:RGB.NET.Devices.CorsairLegacy.CorsairMousepadRGBDevice" />.</param>
+    /// <param name="nativeInfo">The native <see cref="T:RGB.NET.Devices.CorsairLegacy.Native._CorsairDeviceInfo" />-struct</param>
+    internal CorsairMousepadRGBDeviceInfo(int deviceIndex, _CorsairDeviceInfo nativeInfo)
+        : base(deviceIndex, RGBDeviceType.Mousepad, nativeInfo)
+    { }
+
+    #endregion
+}
