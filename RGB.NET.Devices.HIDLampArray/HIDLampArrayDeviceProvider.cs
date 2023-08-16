2647d8059adaddd3f8f9077fae03fc2ba0d5049b
Darth Affe <darthaffe@wyrez.org>
2023-08-16 21:09:20 +0200

    Added LampArray-project (WIP)

---
+++ RGB.NET.Devices.HIDLampArray/HIDLampArrayDeviceProvider.cs
@@ -0,0 +1,61 @@
+﻿// ReSharper disable MemberCanBePrivate.Global
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using HidSharp;
+using HidSharp.Reports;
+using RGB.NET.Core;
+using RGB.NET.HID;
+
+namespace RGB.NET.Devices.HIDLampArray;
+
+/// <inheritdoc />
+/// <summary>
+/// Represents a device provider responsible for PicoPi-devices.
+/// </summary>
+// ReSharper disable once InconsistentNaming
+public sealed class HIDLampArrayDeviceProvider : AbstractRGBDeviceProvider
+{
+    #region Properties & Fields
+
+    private static HIDLampArrayDeviceProvider? _instance;
+    /// <summary>
+    /// Gets the singleton <see cref="HIDLampArrayDeviceProvider"/> instance.
+    /// </summary>
+    public static HIDLampArrayDeviceProvider Instance => _instance ?? new HIDLampArrayDeviceProvider();
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HIDLampArrayDeviceProvider"/> class.
+    /// </summary>
+    /// <exception cref="InvalidOperationException">Thrown if this constructor is called even if there is already an instance of this class.</exception>
+    public HIDLampArrayDeviceProvider()
+    {
+        if (_instance != null) throw new InvalidOperationException($"There can be only one instance of type {nameof(HIDLampArrayDeviceProvider)}");
+        _instance = this;
+    }
+
+    #endregion
+
+    #region Methods
+
+    protected override void InitializeSDK()
+    {
+    }
+
+    protected override IEnumerable<IRGBDevice> LoadDevices()
+    {
+        foreach (HidDevice? device in DeviceList.Local.GetHidDevices())
+        {
+
+        }
+
+        yield break;
+    }
+
+    #endregion
+}
---
+++ RGB.NET.Devices.HIDLampArray/ReadMe.md
@@ -0,0 +1,11 @@
+﻿[RGB.NET](https://github.com/DarthAffe/RGB.NET) Device-Provider-Package for RGB.NET-Devices based on the LampArray-HID-Standard.
+
+## Usage
+This provider follows the default pattern and does not require additional setup.
+
+```csharp
+surface.Load(HIDLampArrayDeviceProvider.Instance);
+```
+
+# Required SDK
+This provider does not require an additional SDK.
