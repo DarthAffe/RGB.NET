67672be54c44051b16fda2125b7ca15cd10a19df
Darth Affe <darthaffe@wyrez.org>
2024-01-17 23:02:02 +0100

    Added Provider for WLED-Devices

---
+++ RGB.NET.Devices.WLED/Generic/WLedRGBDeviceInfo.cs
@@ -0,0 +1,52 @@
+﻿using RGB.NET.Core;
+
+namespace RGB.NET.Devices.WLED;
+
+/// <inheritdoc />
+/// <summary>
+/// Represents a generic information for a WLED-<see cref="T:RGB.NET.Core.IRGBDevice" />.
+/// </summary>
+public sealed class WledRGBDeviceInfo : IRGBDeviceInfo
+{
+    #region Properties & Fields
+
+    /// <inheritdoc />
+    public RGBDeviceType DeviceType => RGBDeviceType.LedStripe;
+
+    /// <inheritdoc />
+    public string DeviceName { get; }
+
+    /// <inheritdoc />
+    public string Manufacturer { get; }
+
+    /// <inheritdoc />
+    public string Model { get; }
+
+    /// <inheritdoc />
+    public object? LayoutMetadata { get; set; }
+
+    /// <summary>
+    /// Gets some info returned by the WLED-device.
+    /// </summary>
+    public WledInfo Info { get; }
+
+    #endregion
+
+    #region Constructors
+
+    /// <summary>
+    /// Internal constructor of managed <see cref="WledRGBDeviceInfo"/>.
+    /// </summary>
+    /// <param name="manufacturer">The manufacturer of the device.</param>
+    /// <param name="model">The represented device model.</param>
+    internal WledRGBDeviceInfo(WledInfo info, string? manufacturer, string? model)
+    {
+        this.Info = info;
+        this.Manufacturer = manufacturer ?? info.Brand;
+        this.Model = model ?? info.Name;
+
+        DeviceName = DeviceHelper.CreateDeviceName(Manufacturer, Model);
+    }
+
+    #endregion
+}
---
+++ RGB.NET.Devices.WLED/README.md
@@ -0,0 +1,18 @@
+﻿[RGB.NET](https://github.com/DarthAffe/RGB.NET) Device-Provider-Package for [WLED](https://kno.wled.ge/)-devices.
+
+## Usage
+This provider does not load anything by default and requires additional configuration to work.
+
+```csharp
+// Add as many WLED-devices as you like
+WledDeviceProvider.Instance.AddDeviceDefinition(new WledDeviceDefinition("<hostname>"));
+
+surface.Load(WledDeviceProvider.Instance);
+```
+
+You can also override the manufacturer and device model in the DeviceDefinition.
+
+# Required SDK
+This provider does not require an additional SDK.
+
+UDP realtime needs to be enabled on the WLED device.
